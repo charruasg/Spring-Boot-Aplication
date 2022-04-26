bc39bfbb79792591824948e466bd964e07b502da
charruasg <myselffp@gmail.com>
2022-04-25 23:43:25 -0300

    Commit inicial con eclipse. Close #1

---
+++ GestiondeForraje/src/test/java/com/GF/GestiondeForraje/GestiondeForrajeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GF.GestiondeForraje;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestiondeForrajeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
